c3c5669e0f61d9aecd7007d89c77941254c1d8c6
dot-1q <tiagobarros99@ua.pt>
2022-12-20 19:46:26 +0000

    real time audio stream, and video is stream simulated

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from pydicom import dcmread
 from PIL import Image
-from io import BytesIO
+import io
 import socket
 import struct
 import time
+import sounddevice as sd
+from threading import Thread
+from scipy.io.wavfile import write
 
 class Server(object):
     def __init__(self):
@@ -21,46 +24,32 @@ class Server(object):
 
     def create_socket(self):
        self.server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-       self.server_socket2= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
        #Dentro da rede da UA isto provavelmente tem de ser mudado
-       port = 9002
+       port = 9000
        socket_address = ('',port)
        print('Video Socket Created')
        self.server_socket.bind(socket_address)
        print('Video Socket Binded')
-       self.server_socket.listen(5)
+       self.server_socket.listen(10)
        print('Video Socket Listening')
 
-       #Dentro da rede da UA isto provavelmente tem de ser mudado
-       port = 9003
-       socket_address2 = ('',port)
-       print('Audio Socket Created')
-       self.server_socket2.bind(socket_address2)
-       print('Audio Socket Binded')
-       self.server_socket2.listen(5)
-       print('Audio Socket Listening')
-
-
     def stream_video(self):
+        print("Created video stream")
         client_socket,addr = self.server_socket.accept()
         print('Connection from:', addr)
         
-        print("Waiting for 2 conn")
-        client_socket2 ,addr2 = self.server_socket2.accept()
-        print('Connection from:', addr2)
         while True:
-            if client_socket and client_socket2:
+            if client_socket:
                 image = Image.fromarray(self.frames[self.counter])
                 if self.counter < len(self.frames)-1:
                     self.counter +=1
                 else:
                     self.counter=0
-                buf = BytesIO()
+                buf = io.BytesIO()
                 image.save(buf,'JPEG')
                 message = struct.pack("Q",len(buf.getbuffer()))+buf.getbuffer()
                 client_socket.sendall(message)
-                client_socket2.sendall(message)
                 print("Sent Frame {n}".format(n=self.counter))
                 time.sleep(0.2)
 
--- test.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from pydicom import dcmread
-
-ds = dcmread('bio.dcm')
-for i in range(ds.NumberOfFrames):
-    frame = ds.pixel_array[i]
-    print(type(frame))
-    plt.imshow(frame)
-    plt.show()
