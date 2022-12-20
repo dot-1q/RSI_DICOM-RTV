a6b811fab290d23e61b2015c5f2f9e80f06c0a22
dot-1q <tiagobarros99@ua.pt>
2022-12-20 12:29:01 +0000

    Sends two seperate video streams, just to test that its possible. One of the streams is getting converted into audio

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import socket
 import struct
 
 app = Flask(__name__, static_url_path="/static")
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host_ip = '127.0.0.1'
-port = 9001
+port = 9002
 
-client_socket.connect((host_ip,port))
+client_socket1.connect((host_ip,port))
+client_socket2.connect((host_ip,port+1))
 payload_size = struct.calcsize("Q")
 
 @app.route("/", methods=["GET", "POST"])
@@ -19,23 +21,47 @@ def gen():
     data = b''
     while True:
         while len(data) < payload_size:
-            packet = client_socket.recv(4*1024)
+            packet = client_socket1.recv(4*1024)
             if not packet: break
             data+=packet
         packed_msg_size = data[:payload_size]
         data = data[payload_size:]
         msg_size = struct.unpack("Q",packed_msg_size)[0]
         while len(data) < msg_size:
-            data += client_socket.recv(4*1024)
+            data += client_socket1.recv(4*1024)
         frame_data = data[:msg_size]
         data = data[msg_size:]
         frame = frame_data
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + (frame) + b'\r\n')
 
+def gen2():
+    data = b''
+    while True:
+        while len(data) < payload_size:
+            packet = client_socket2.recv(4*1024)
+            if not packet: break
+            data+=packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q",packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += client_socket2.recv(4*1024)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        frame = frame_data
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + (frame) + b'\r\n')
+
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video_feed2')
+def video_feed2():
+    return Response(gen2(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -21,22 +21,36 @@ class Server(object):
 
     def create_socket(self):
        self.server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+       self.server_socket2= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
        #Dentro da rede da UA isto provavelmente tem de ser mudado
-       port = 9001
+       port = 9002
        socket_address = ('',port)
-       print('Socket Created')
+       print('Video Socket Created')
        self.server_socket.bind(socket_address)
-       print('Socket Binded')
+       print('Video Socket Binded')
        self.server_socket.listen(5)
-       print('Socket Listening')
+       print('Video Socket Listening')
+
+       #Dentro da rede da UA isto provavelmente tem de ser mudado
+       port = 9003
+       socket_address2 = ('',port)
+       print('Audio Socket Created')
+       self.server_socket2.bind(socket_address2)
+       print('Audio Socket Binded')
+       self.server_socket2.listen(5)
+       print('Audio Socket Listening')
 
 
     def stream_video(self):
         client_socket,addr = self.server_socket.accept()
         print('Connection from:', addr)
+        
+        print("Waiting for 2 conn")
+        client_socket2 ,addr2 = self.server_socket2.accept()
+        print('Connection from:', addr2)
         while True:
-            if client_socket:
+            if client_socket and client_socket2:
                 image = Image.fromarray(self.frames[self.counter])
                 if self.counter < len(self.frames)-1:
                     self.counter +=1
@@ -46,7 +60,9 @@ class Server(object):
                 image.save(buf,'JPEG')
                 message = struct.pack("Q",len(buf.getbuffer()))+buf.getbuffer()
                 client_socket.sendall(message)
+                client_socket2.sendall(message)
                 print("Sent Frame {n}".format(n=self.counter))
                 time.sleep(0.2)
 
+# Run the server
 Server()
